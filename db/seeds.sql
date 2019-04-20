11a5b6773f36b91c7a8dddc6b6062bd2bddc9fb3
Dylan Davidson <rogue1037@gmail.com>
2019-04-19 21:28:32 -0700

    added server data and connection.js to config

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, burger_devoured) VALUES ('Bacon burger', false);
+INSERT INTO burgers (burger_name, burger_devoured) VALUES ('American burger', false);
+INSERT INTO burgers (burger_name, burger_devoured) VALUES ('Royal Red Robin burger', false);
